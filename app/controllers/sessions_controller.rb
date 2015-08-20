8dd123fd8c7886f8f6b485f76ad243c3a0b3774b
Александр <aleksandr.misyura@gmail.com>
2015-08-20 21:35:51 +0000

    sample_app: before change _header

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+  
+  def new
+  
+  end
+
+  def create
+    #render 'new'
+    #render text: params[:session][:password]
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      #render text: "OK!"
+    else
+      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   get 'signup' => 'users#new'
   
+  # from sessions controller
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
