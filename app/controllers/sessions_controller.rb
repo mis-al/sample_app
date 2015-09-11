ab1ac08d8d6b0da797086293e06fefa4f068b2b4
Александр <aleksandr.misyura@gmail.com>
2015-09-11 16:22:31 +0000

    Before Showing all users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
-      redirect_to user
+      redirect_back_or user
+      #remember user
+      #redirect_to user
       #render text: "OK!"
     else
       # Create an error message.
