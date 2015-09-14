4049facf1cdb0f4d905801e179d1c247ba14dea0
Александр <aleksandr.misyura@gmail.com>
2015-09-14 20:48:38 +0000

    Add account activations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     if @user.save
       # Handle a successful save.
       
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
       
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,14 +34,33 @@ class User < ActiveRecord::Base
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # olding version method :)
+  #def authenticated?(remember_token)
+  #  BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  #end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Forgets a user.
   def forget
     return false if remember_digest.nil?
     update_attribute(:remember_digest, nil)
+  end
+  
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true )
+    update_attribute(:activated_at, Time.zone.now)  
+  end
+  
+  # Send activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
   
   private
@@ -56,5 +75,7 @@ class User < ActiveRecord::Base
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
+  
+  
   
 end
