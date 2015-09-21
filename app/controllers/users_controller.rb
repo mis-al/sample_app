e7a569049d1109c1b13300a406ab887e7a9096f7
Александр <aleksandr.misyura@gmail.com>
2015-09-21 09:36:18 +0000

    Before Micropost images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,9 @@ class UsersController < ApplicationController
   def show 
     @user =  User.find(params[:id])
    # debugger
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+
   end
   
   def new
@@ -64,13 +67,14 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
     
-     def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    # This method implemented in aplication _controller
+    # def logged_in_user
+    #   unless logged_in?
+    #     store_location
+    #     flash[:danger] = "Please log in."
+    #     redirect_to login_url
+    #   end
+    # end
     
     # Confirms the correct user.
     def correct_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,13 @@ User.create!( name:                   "Example User",
                 activated:              true,
                 activated_at:           Time.zone.now )
 end
+
+users = User.order(:created_at).take(10)
+
+50.times do |i|
+  puts "#{i} item from each user"
+  content = Faker::Lorem.sentence(5)
+  users.each do |user|
+    user.microposts.create!(content: content)
+  end
+end
